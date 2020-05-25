304fd45433aedadd79ef80abdb38ff8f594c1736
Fluhzar <fluhzar@pm.me>
2020-05-24 17:54:27 -0700

    Improved gain code by a lot

--- Code/gain-test/src/main.rs
+++ Code/gain-test/src/main.rs
@@ -1,351 +1,47 @@
-use wav;
+// use crate gain_test;
+use gain::*;
 
-type SampleT = f32;
-type MathT = f64;
+/// Size of the processing chunk
+const CHUNK_SIZE: usize = 1 << 10;
 
-#[derive(Debug,Default,Copy,Clone)]
-/// Struct representing a stereophonic audio sample
-pub struct StereoData{
-	left:SampleT,
-	right:SampleT,
-}
-
-impl StereoData {
-	/// Returns a new StereoData object created from individual left and right
-	/// audio samples.
-	/// 
-	/// # Parameters
-	/// 
-	/// * `l` - the left audio sample.
-	/// * `r` - the right audio sapmle.
-	pub fn from_stereo(l:SampleT, r:SampleT) -> StereoData {
-		StereoData{
-			left:l,
-			right:r
-		}
-	}
-
-	/// Creates a new StereoData object from a single monophonic sample. This
-	/// function reduces the power of the given sample by half to reflect human
-	/// hearing.
-	/// 
-	/// # Parameters
-	/// 
-	/// * `x` - the input sample.
-	pub fn from_mono(x:SampleT) -> StereoData {
-		StereoData{
-			left: SampleT::sqrt(0.5)*x,
-			right: SampleT::sqrt(0.5)*x
-		}
-	}
-
-	/// Converts the given stereophonic sample to a monophonic sample by summing
-	/// the left and right samples and dividing by half power to get the full
-	/// power monophonic sample.
-	pub fn as_mono(&self) -> SampleT {
-		(self.left + self.right)/SampleT::sqrt(0.5)
-	}
-
-	/// Returns the left audio sample.
-	pub fn left(&self) -> SampleT {
-		self.left
-	}
-
-	/// Returns the right audio sample.
-	pub fn right(&self) -> SampleT {
-		self.right
-	}
-}
-
-impl std::ops::Neg for StereoData {
-	type Output = Self;
-
-	fn neg(self) -> Self::Output {
-		StereoData {
-			left: -self.left,
-			right: -self.right,
-		}
-	}
-}
-
-impl std::ops::Add<StereoData> for StereoData {
-	type Output = Self;
-
-	fn add(self, rhs: StereoData) -> Self::Output {
-		StereoData {
-			left: self.left + rhs.left,
-			right: self.right + rhs.right,
-		}
-	}
-}
-
-impl std::ops::AddAssign<StereoData> for StereoData {
-	fn add_assign(&mut self, rhs: StereoData) {
-		self.left += rhs.left;
-		self.right += rhs.right;
-	}
-}
-
-impl std::ops::Sub<StereoData> for StereoData {
-	type Output = Self;
-
-	fn sub(self, rhs: StereoData) -> Self {
-		StereoData {
-			left: self.left - rhs.left,
-			right: self.right - rhs.right,
-		}
-	}
-}
-
-impl std::ops::SubAssign<StereoData> for StereoData {
-	fn sub_assign(&mut self, rhs: StereoData) {
-		self.left -= rhs.left();
-		self.right -= rhs.right();
-	}
-}
-
-impl std::ops::Mul<SampleT> for StereoData {
-	/// Output type of the multiplication
-	type Output = StereoData;
-
-	/// Multiplies a sample by a value. E.g. scaling the sample by a gain amount.
-	fn mul(self, rhs: SampleT) -> Self::Output {
-		StereoData {
-			left: self.left * rhs,
-			right: self.right * rhs,
-		}
-	}
-}
-impl std::ops::Mul<StereoData> for SampleT {
-	type Output = StereoData;
-
-	fn mul(self, rhs: StereoData) -> Self::Output {
-		StereoData {
-			left: self * rhs.left,
-			right: self * rhs.right,
-		}
-	}
-}
-
-impl std::ops::MulAssign<SampleT> for StereoData {
-	fn mul_assign(&mut self, rhs: SampleT) {
-		self.left *= rhs;
-		self.right *= rhs;
-	}
-}
-
-impl std::ops::Mul<MathT> for StereoData {
-	/// Output type of the multiplication
-	type Output = StereoData;
-
-	/// Multiplies a sample by a value. E.g. scaling the sample by a gain amount.
-	fn mul(self, rhs: MathT) -> Self::Output {
-		StereoData {
-			left:(self.left as MathT * rhs) as SampleT,
-			right:(self.right as MathT * rhs) as SampleT,
-		}
-	}
-}
-impl std::ops::Mul<StereoData> for MathT {
-	type Output = StereoData;
-
-	fn mul(self, rhs: StereoData) -> Self::Output {
-		StereoData {
-			left: self as SampleT * rhs.left,
-			right: self as SampleT * rhs.right
-		}
-	}
-}
-
-impl std::ops::MulAssign<MathT> for StereoData {
-	fn mul_assign(&mut self, rhs: MathT) {
-		self.left *= rhs as SampleT;
-		self.right *= rhs as SampleT;
-	}
-}
-
-impl Into<Vec<u8>> for StereoData {
-	/// Converts the StereoData into a vector of bytes.
-	fn into(self) -> Vec<u8> {
-		let mut v = Vec::new();
-
-			// Converts the left sample from SampleT (f32) to i16, then to bytes
-		let n = ((self.left * 0x80_00 as SampleT) as i16).to_le_bytes();
-		v.push(n[0]);
-		v.push(n[1]);
-
-			// Converts the right sample from SampleT (f32) to i16, then to bytes
-		let n = ((self.right * 0x80_00 as SampleT) as i16).to_le_bytes();
-		v.push(n[0]);
-		v.push(n[1]);
-
-		v
-	}
-}
-
-impl From<[u8;2]> for StereoData {
-	/// Converts the array of 2 bytes into a StereoData object.
-	/// It is assumed that the bytes are 8-bit unsigned audio samples.
-	/// 
-	/// # Parameters
-	/// 
-	/// * `v` - The raw bytes to convert from.
-	fn from(v:[u8;2]) -> Self {
-		StereoData {
-			left: sample_from_u8([v[0]]),
-			right: sample_from_u8([v[1]])
-		}
-	}
-}
-
-impl From<[u8;4]> for StereoData {
-	/// Converts the array of 4 bytes into a StereoData object.
-	/// It is assumed that the bytes are 16-bit signed audio samples.
-	/// 
-	/// # Parameters
-	/// 
-	/// * `v` - The raw bytes to convert from.
-	fn from(v:[u8;4]) -> Self {
-		StereoData {
-			left: sample_from_i16([v[0],v[1]]),
-			right: sample_from_i16([v[2],v[3]])
-		}
-	}
-}
-
-impl From<[u8;6]> for StereoData {
-	/// Converts the array of 6 bytes into a StereoData object.
-	/// It is assumed that the bytes are 24-bit signed audio samples.
-	/// 
-	/// # Parameters
-	/// 
-	/// * `v` - The raw bytes to convert from.
-	fn from(v:[u8;6]) -> Self {
-		StereoData {
-			left:  sample_from_i24([v[0],v[1],v[2]]),
-			right: sample_from_i24([v[3],v[4],v[5]])
-		}
-	}
-}
-
-/// Converts a raw bytes to a Sample
-/// It is assumed the bytes are 8-bit unsigned audio samples.
-/// 
-/// # Parameters
-/// 
-/// * `v` - The raw bytes to convert from.
-pub fn sample_from_u8(v:[u8;1]) -> SampleT {
-	(v[0] as SampleT - 128.0) / 128.0
-}
-
-/// Converts raw bytes to a Sample
-/// It is assumed that the bytes are 16-bit signed audio samples.
-/// 
-/// # Parameters
-/// 
-/// * `v` - The raw bytes to convert from.
-pub fn sample_from_i16(v:[u8;2]) -> SampleT {
-	(i16::from_le_bytes(v) as SampleT) / (0x8000 as SampleT)
-}
-
-pub fn sample_from_i16_val(v: i16) -> SampleT {
-	sample_from_i16(v.to_le_bytes())
-}
-
-/// Converts raw bytes to a Sample
-/// It is assumed that the bytes are 24-bit signed audio samples.
-/// 
-/// # Parameters
-/// 
-/// * `v` - The raw bytes to convert from.
-pub fn sample_from_i24(v:[u8;3]) -> SampleT {
-	(i32::from_le_bytes([v[0],v[1],v[2],0]) as SampleT) / (0x800000 as SampleT)
-}
-
-/// Converts a decibel value to a linear gain value.
-/// This assumes that 0dB is unity gain, and ~-6bB is 0.5 gain
-pub fn db_linear(db:MathT) -> MathT {
-	10.0_f64.powf(db/20.0)
-}
-
-/// Converts a linear gain value to a decibel value.
-/// This assumes that 0dB is unity gain, and ~-6bB is 0.5 gain
-pub fn linear_db(g:MathT) -> MathT {
-	20.0 * g.log10()
-}
-
-type TrackT = Vec<StereoData>;
-
-fn vec2track(v: Vec<i16>) -> TrackT {
-	let mut t = TrackT::new();
-
-	let mut i = 0;
-	while i < v.len() {
-		t.push(
-			StereoData::from_stereo(
-				sample_from_i16_val(v[i]),
-				sample_from_i16_val(v[i+1])
-			)
-		);
-
-		i+=2;
-	}
-
+fn calc_gain(t: SampleTrackT) -> SampleTrackT {
 	t
+		.chunks_exact(CHUNK_SIZE)
+		.flat_map(
+			|x| {
+				let mut largest = 0.0;
+				for s in x {
+					if s.abs() > largest {
+						largest = s.abs();
+					}
+				}
+				vec![largest ; CHUNK_SIZE]
+			}
+		).collect()
 }
 
-fn track2vec(t: TrackT) -> Vec<i16> {
-	let mut v = Vec::new();
-
-	for s in t {
-		let b:Vec<u8> = s.into();
-		v.push(i16::from_le_bytes([b[0], b[1]]));
-		v.push(i16::from_le_bytes([b[2], b[3]]));
-	}
-
-	v
-}
-
-fn main() {
+fn main() -> Result<(),std::io::Error>{
 	for f in std::env::args() {
 		match f.find(".wav") {
 			Some(_) => {
-				let (w,t) = wav::read_file(std::path::Path::new(f.as_str())).unwrap();
-
-				println!("{:?}", w);
-
-				match t {
-					wav::BitDepth::Sixteen(v) => {
-						let t = vec2track(v);
-
-						let mut largest = 0.0;
-						let mut i = 0;
-						let mut gain = TrackT::new();
-
-						while i < t.len()-((w.sampling_rate/100) as usize) {
-							for s in 0..(w.sampling_rate/100) {
-								let g = t[s as usize + i].left().abs();
-
-								if g > largest {
-									largest = g;
-								}
-
-								// println!("g: {} \t largest:{}", g, largest);
-							}
-
-							for _ in 0..(w.sampling_rate/100) {
-								gain.push(StereoData::from_stereo(largest, largest));
-							}
-							largest = 0.0;
-							i += (w.sampling_rate/100) as usize;
-						}
-
-						wav::write_wav(w, wav::BitDepth::Sixteen(track2vec(gain)), &std::path::Path::new("gain.wav")).unwrap();
-					}
-					_ => ()
+				let (w, t) = {
+					let mut file = std::fs::File::open(f)?;
+					let (w, mut t) = read_wav(&mut file)?;
+					(w, t.pop().unwrap())
 				};
+
+				let t = calc_gain(t);
+
+				let mut file = std::fs::File::create("gain.wav")?;
+				WaveWriteOptions::new()
+					.bps(16).unwrap()
+					.r(w.sampling_rate as MathT)
+					.clip(false)
+					.write(vec![t], &mut file)?;
 			},
 			None => (),
 		};
 	}
+
+	Ok(())
 }
---
+++ Code/gain-test/src/stereo.rs
@@ -0,0 +1,274 @@
+//! # Stereo
+//!
+//! Module containing type for handling stereophonic audio data.
+
+use super::*;
+
+/// Type for a track of [`Stereo`] samples
+///
+/// [`Stereo`]: struct.Stereo.html
+pub type StereoTrackT = Vec<Stereo>;
+
+/// Struct representing a stereophonic audio sample.
+#[derive(Debug, Copy, Clone, Default, PartialEq)]
+#[repr(C)]
+pub struct Stereo {
+    /// Left sample value.
+    pub left: SampleT,
+    /// Right sample value.
+    pub right: SampleT,
+}
+
+impl Stereo {
+    /// Returns a new Stereo object with default <0,0> values.
+    pub fn new() -> Self {
+        Stereo::default()
+    }
+
+    /// Returns a new Stereo object created from individual left and right
+    /// audio samples.
+    ///
+    /// # Parameters
+    ///
+    /// * `l` - the left audio sample.
+    /// * `r` - the right audio sample.
+    pub fn from(l: SampleT, r: SampleT) -> Self {
+        Stereo { left: l, right: r }
+    }
+}
+
+impl SampleFormat for Stereo {
+    fn from_sample(x: SampleT) -> Self {
+        Stereo {
+            left: x * SampleT::sqrt(0.5),
+            right: x * SampleT::sqrt(0.5),
+        }
+    }
+
+    fn into_sample(self) -> SampleT {
+        (self.left + self.right) / SampleT::sqrt(0.5)
+    }
+
+    fn num_samples() -> usize {
+        2
+    }
+}
+
+/// Pans a given sample between the left and right channels. The panning
+/// parameter `g` is a floating point value of the rang \[-1,1\], where -1 is
+/// panned full left and 1 is panned full right. If the given value is not
+/// within this range, it is clamped to it.
+impl Panner<f32> for Stereo {
+    fn to_sample_format(s: SampleT, g: f32) -> Self {
+        let l_lerp = if g <= 0.0 {
+            clerp(g as MathT, -1.0, 0.0, -3.0, -120.0)
+        } else {
+            clerp(g as MathT, 0.0, 1.0, 0.0, -3.0)
+        };
+        let r_lerp = if g >= 0.0 {
+            clerp(g as MathT, 0.0, 1.0, -3.0, -120.0)
+        } else {
+            clerp(g as MathT, -1.0, 0.0, 0.0, -3.0)
+        };
+
+        Stereo {
+            left: (db_to_linear(l_lerp) * s as MathT) as SampleT,
+            right: (db_to_linear(r_lerp) * s as MathT) as SampleT,
+        }
+    }
+}
+impl Panner<f64> for Stereo {
+    fn to_sample_format(s: SampleT, g: f64) -> Self {
+        let l_lerp = if g <= 0.0 {
+            clerp(g as MathT, -1.0, 0.0, -3.0, -120.0)
+        } else {
+            clerp(g as MathT, 0.0, 1.0, 0.0, -3.0)
+        };
+        let r_lerp = if g >= 0.0 {
+            clerp(g as MathT, 0.0, 1.0, -3.0, -120.0)
+        } else {
+            clerp(g as MathT, -1.0, 0.0, 0.0, -3.0)
+        };
+
+        Stereo {
+            left: (db_to_linear(l_lerp) * s as MathT) as SampleT,
+            right: (db_to_linear(r_lerp) * s as MathT) as SampleT,
+        }
+    }
+}
+
+impl std::ops::Neg for Stereo {
+    type Output = Self;
+
+    fn neg(self) -> Self::Output {
+        Stereo {
+            left: -self.left,
+            right: -self.right,
+        }
+    }
+}
+
+impl std::ops::Add<Stereo> for Stereo {
+    type Output = Self;
+
+    fn add(self, rhs: Stereo) -> Self::Output {
+        Stereo {
+            left: self.left + rhs.left,
+            right: self.right + rhs.right,
+        }
+    }
+}
+impl std::ops::AddAssign<Stereo> for Stereo {
+    fn add_assign(&mut self, rhs: Stereo) {
+        self.left += rhs.left;
+        self.right += rhs.right;
+    }
+}
+
+impl std::ops::Sub<Stereo> for Stereo {
+    type Output = Self;
+
+    fn sub(self, rhs: Stereo) -> Self {
+        Stereo {
+            left: self.left - rhs.left,
+            right: self.right - rhs.right,
+        }
+    }
+}
+impl std::ops::SubAssign<Stereo> for Stereo {
+    fn sub_assign(&mut self, rhs: Stereo) {
+        self.left -= rhs.left;
+        self.right -= rhs.right;
+    }
+}
+
+impl std::ops::Mul<Stereo> for Stereo {
+    type Output = Stereo;
+
+    fn mul(self, rhs: Stereo) -> Self::Output {
+        Stereo {
+            left: self.left * rhs.left,
+            right: self.right * rhs.right,
+        }
+    }
+}
+impl std::ops::MulAssign<Stereo> for Stereo {
+    fn mul_assign(&mut self, rhs: Stereo) {
+        self.left *= rhs.left;
+        self.right *= rhs.right;
+    }
+}
+
+impl std::ops::Mul<SampleT> for Stereo {
+    type Output = Stereo;
+
+    fn mul(self, rhs: SampleT) -> Self::Output {
+        Stereo {
+            left: self.left * rhs,
+            right: self.right * rhs,
+        }
+    }
+}
+impl std::ops::MulAssign<SampleT> for Stereo {
+    fn mul_assign(&mut self, rhs: SampleT) {
+        self.left *= rhs;
+        self.right *= rhs;
+    }
+}
+
+impl std::ops::Mul<MathT> for Stereo {
+    type Output = Stereo;
+
+    fn mul(self, rhs: MathT) -> Self::Output {
+        Stereo {
+            left: (self.left as MathT * rhs) as SampleT,
+            right: (self.right as MathT * rhs) as SampleT,
+        }
+    }
+}
+impl std::ops::MulAssign<MathT> for Stereo {
+    fn mul_assign(&mut self, rhs: MathT) {
+        self.left *= rhs as SampleT;
+        self.right *= rhs as SampleT;
+    }
+}
+
+impl From<SampleT> for Stereo {
+    fn from(s: SampleT) -> Self {
+        Stereo::from_sample(s)
+    }
+}
+impl Into<SampleT> for Stereo {
+    fn into(self) -> SampleT {
+        self.into_sample()
+    }
+}
+
+impl TryFrom<Vec<u8>> for Stereo {
+    type Error = String;
+
+    fn try_from(v: Vec<u8>) -> Result<Self, Self::Error> {
+        if v.len() < 2 {
+            Err(format!(
+                "ERROR: Given vector was length {}. This function requires length 2.",
+                v.len()
+            ))
+        } else {
+            Ok(Stereo {
+                left: sample_from_u8(v[0]),
+                right: sample_from_u8(v[1]),
+            })
+        }
+    }
+}
+impl Into<Vec<u8>> for Stereo {
+    fn into(self) -> Vec<u8> {
+        vec![sample_to_u8(self.left), sample_to_u8(self.right)]
+    }
+}
+
+impl TryFrom<Vec<i16>> for Stereo {
+    type Error = String;
+
+    fn try_from(v: Vec<i16>) -> Result<Self, Self::Error> {
+        if v.len() < 2 {
+            Err(format!(
+                "ERROR: Given vector was length {}. This function requires length 2.",
+                v.len()
+            ))
+        } else {
+            Ok(Stereo {
+                left: sample_from_i16(v[0]),
+                right: sample_from_i16(v[1]),
+            })
+        }
+    }
+}
+impl Into<Vec<i16>> for Stereo {
+    fn into(self) -> Vec<i16> {
+        vec![sample_to_i16(self.left), sample_to_i16(self.right)]
+    }
+}
+
+impl TryFrom<Vec<i32>> for Stereo {
+    type Error = String;
+
+    fn try_from(v: Vec<i32>) -> Result<Self, Self::Error> {
+        if v.len() < 2 {
+            Err(format!(
+                "ERROR: Given vector was length {}. This function requires length 2.",
+                v.len()
+            ))
+        } else {
+            Ok(Stereo {
+                left: sample_from_i24(v[0]),
+                right: sample_from_i24(v[1]),
+            })
+        }
+    }
+}
+impl Into<Vec<i32>> for Stereo {
+    fn into(self) -> Vec<i32> {
+        vec![sample_to_i24(self.left), sample_to_i24(self.right)]
+    }
+}
