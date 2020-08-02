e7fe8a0733f457c9780ebf65571aa183500e1c25
Fluhzar <fluhzar@pm.me>
2020-08-02 14:18:35 -0700

    Updated APA102CPiDraw to be compliant with both APA102C and SK9822 LEDs.

--- Code/base/src/draw/pi_draw.rs
+++ Code/base/src/draw/pi_draw.rs
@@ -6,12 +6,28 @@ use crate::util::{frame::Frame, rgb::*};
 use rppal::gpio;
 use std::cell::RefCell;
 
-/// Type used to represent a GPIO pin with interior mutability. This is required bc iterating over a frame borrows `self`, and setting the pin values requires mutation through `self` inside the iteration.
+/// Type used to represent a GPIO pin with interior mutability. This is required bc iterating over a frame borrows `self`, and
+/// setting the pin values requires mutation through `self` inside the iteration.
 pub type Pin = RefCell<gpio::OutputPin>;
+
+/// Type alias for the SK9822 LED, which is a clone of the APA102C and compatible with our implementation of the APA102C's data
+/// transmission protocol.
+pub type SK9822PiDraw = APA102CPiDraw;
 
 /// Struct that draws [APA102C][0] LEDs through the Raspberry Pi's GPIO pins.
 /// 
+/// This struct is also compatible with the SK9822 LEDs, which are more or less a clone of the APA102C LED, though there are
+/// some notable differences seen [here][1] that are accounted for in this struct.
+/// 
+/// For APA102C LEDs, it generally isn't recommended to have the brightness set to anything other than full as the PWM that
+/// handles the brightness runs at 440Hz, which can cause flicker issues on lower brightness settings. The SK9822 clone gets
+/// around this issue by current-limiting according to the brightness value instead of adjusting PWM settings.
+/// 
+/// NOTE TO FUTURE SELF: As both the start and end frame both are writing 0s to the data line, why not try combining the start
+/// and end frame into 1 call, and writing n/2 + 32 0s to the data line in one shot.
+/// 
 /// [0]: https://cdn-shop.adafruit.com/datasheets/APA102.pdf
+/// [1]: https://cpldcpu.wordpress.com/2016/12/13/sk9822-a-clone-of-the-apa102/
 #[derive(Debug)]
 pub struct APA102CPiDraw {
     data: Pin,
@@ -39,7 +55,7 @@ impl APA102CPiDraw {
     }
 
     /// The start frame representing the start of a message to the LEDs as
-    /// defined by the [datasheet][0]
+    /// defined by the [datasheet][0].
     /// 
     /// [0]: https://cdn-shop.adafruit.com/datasheets/APA102.pdf
     #[inline]
@@ -54,21 +70,16 @@ impl APA102CPiDraw {
 
     /// The end frame representing the end of a message to the LEDs as defined
     /// by the [datasheet][0] with modifications as revealed in
-    /// [this blog post][1].
+    /// [this blog post][1], and a subsequent [follow-up post][2] discussing the
+    /// APA102C clone, the SK9822.
     /// 
     /// [0]: https://cdn-shop.adafruit.com/datasheets/APA102.pdf
     /// [1]: https://cpldcpu.wordpress.com/2014/11/30/understanding-the-apa102-superled/
+    /// [2]: https://cpldcpu.wordpress.com/2016/12/13/sk9822-a-clone-of-the-apa102/
     #[inline]
-    fn end_frame(&mut self, size: usize) {
-        if size <= 64 {
-            self.write_byte(0xFF);
-            self.write_byte(0xFF);
-            self.write_byte(0xFF);
-            self.write_byte(0xFF);
-        } else {
-            for _ in 0..(size >> 4) {
-                self.write_byte(0xFF);
-            }
+    fn end_frame(&mut self) {
+        for _ in 0..(self.frame.len()>>4) {
+            self.write_byte(0x00);
         }
     }
 
@@ -127,13 +138,13 @@ impl Draw for APA102CPiDraw {
         self.start_frame();
 
         for led in self.frame.iter() {
-            self.write_byte(self.frame.brightness_apa102c());
+            self.write_byte(0xE0 | self.frame.brightness_apa102c());
             let color = led.as_tuple(RGBOrder::BGR);
             self.write_byte(color.0);
             self.write_byte(color.1);
             self.write_byte(color.2);
         }
-        self.end_frame(self.frame.len());
+        self.end_frame();
 
         Ok(())
     }
@@ -146,3 +157,4 @@ impl Draw for APA102CPiDraw {
         self.frame.as_mut_slice()
     }
 }
+F
