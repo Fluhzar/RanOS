dfcc77a1dad15f5c6fc5aac991592f7b2e299e50
Fluhzar <fluhzar@pm.me>
2020-08-13 19:24:12 -0700

    Removed time restriction on stats printing.

--- Code/base_cpp/src/main.cpp
+++ Code/base_cpp/src/main.cpp
@@ -12,7 +12,6 @@
 #define TARGET_DT ::RanOS::Option<::RanOS::Duration>::Some(::RanOS::Duration(1.0 / 144.0))
 #define BRIGHTNESS 0.125
 #define RANDOM false
-#define WAIT ::RanOS::Duration(60)
 
 RanOS::APA102CDraw drawer(DATA_PIN, CLOCK_PIN, TARGET_DT);
 RanOS::Vec<RanOS::RGB> colors{RanOS::RGB::from_hsv(0.0, 1.0, 1.0), RanOS::RGB::from_hsv(30.0, 1.0, 1.0), RanOS::RGB::from_hsv(60.0, 1.0, 1.0), RanOS::RGB::from_hsv(120.0, 1.0, 1.0), RanOS::RGB::from_hsv(210.0, 1.0, 1.0), RanOS::RGB::from_hsv(280.0, 1.0,1.0)};
@@ -20,16 +19,12 @@ RanOS::Option<RanOS::Vec<RanOS::RGB>> order(RANDOM ? RanOS::Option<RanOS::Vec<Ra
 RanOS::Box<RanOS::Breath> breath = NEW_BOX(RanOS::Breath, RanOS::Duration(4*6), RanOS::Duration(4), BRIGHTNESS, NUM_LEDS, order);
 RanOS::Box<RanOS::Rainbow> rainbow = NEW_BOX(RanOS::Rainbow, RanOS::Duration(4 * 6), RanOS::Duration(4), BRIGHTNESS, NUM_LEDS, 1.0, 1.0, 1.0, 1);
 
-RanOS::Duration start;
-
 void setup()
 {
     drawer.stop(NUM_LEDS*NUM_LEDS);
 
     Serial.begin(9600);
     while(!Serial) {}
-
-    start = TIME_NOW();
 }
 
 void loop() {
@@ -37,8 +32,5 @@ void loop() {
     drawer.push_queue(NEW_RC(RanOS::Rainbow, *rainbow));
     drawer.run();
 
-    if((TIME_NOW() - start) > WAIT) {
-        Serial.println(drawer.get_stats().to_string().c_str());
-        start += WAIT;
-    }
+    Serial.println(drawer.get_stats().to_string().c_str());
 }
